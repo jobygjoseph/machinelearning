2267d0655fb529e0ac717620246fa76dd26ae584
Joby Joseph <joby.joseph@nbcuni.com>
2018-01-01 22:19:27 -0500

    Done homework 3 + adding token

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -8,7 +8,9 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
+    hThetaX = 0;
+    summer = 0;
+    
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
     %               theta. 
@@ -18,15 +20,23 @@ for iter = 1:num_iters
     %
 
 
-
-
-
-
-
+    % theta(1 & 2) = theta(1& 2) - alpha * derivative (with respect to J) * costFunction(theta)
+    
+    hThetaX = X * theta;
+    jTheta1 = sum((hThetaX - y)' * X(:,1));
+    jTheta2 = sum((hThetaX - y)' * X(:,2));
+    
+    theta(1) = theta(1) - (alpha / m) * jTheta1;
+    theta(2) = theta(2) - (alpha / m) * jTheta2;
+    
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
+    
+    % disp('J history and theta');
+    % J_history(iter)
+    % theta
 
 end
 
