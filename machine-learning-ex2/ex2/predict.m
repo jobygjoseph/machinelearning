c45ba37437dc23165a1d9f1bc8f379ddec4e2225
jobygjoseph <jobygjoseph@yahoo.com>
2018-01-03 23:01:45 -0500

    Working and grade passing predict.m

--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -15,10 +15,15 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+predictions = sigmoid(X * theta);
 
-
-
-
+for ind = 1:size(predictions)
+  if predictions(ind) >= 0.5
+    p(ind) = 1;
+  else
+    p(ind) = 0;
+  end
+end;
 
 
 % =========================================================================
